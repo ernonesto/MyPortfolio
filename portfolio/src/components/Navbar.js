6372f49b632c25c3ac0418224f8f303a7323c1ff
ernonesto <ernonesta1616@yahoo.com>
2023-01-28 12:20:03 -0500

    Add styles

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,21 +1,27 @@
+import { useRef } from "react";
 import {FaBars, FaTimes} from "react-icons/fa";
 
 function Navbar(){
+    const navRef = useRef();
+
+    const showNavbar = () =>{
+        navRef.current.classList.toggle("responsive_nav");
+    }
     return( 
         <header>
             <h3>My Pics</h3>
-            <nav>
+            <nav ref = {navRef}>
                 <a href="/#">About me</a>
                 <a href="/#">My School</a>
                 <a href="/#">My Experience</a>
                 <a href="/#">My Linkedin</a>
                 <a href="/#">My work</a>
-                <button>
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
                     <FaTimes/>
                 </button>
 
             </nav>
-            <button>
+            <button className="nav-btn" onClick={showNavbar}>
                 <FaBars/>
             </button>
         </header>
